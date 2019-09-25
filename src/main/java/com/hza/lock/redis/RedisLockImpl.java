8fd0522ed57dd9ebafe593601d878ac7185517f5
黄志安 <zhianhuang@outlook.com.com>
2019-09-25 13:37:03 +0800

    RedisLock实现和测试

--- src/main/java/com/hza/lock/redis/RedisLockImpl.java
+++ src/main/java/com/hza/lock/redis/RedisLockImpl.java
@@ -1,9 +1,7 @@
 package com.hza.lock.redis;
 
 import com.hza.lock.entity.LockResource;
-import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisCluster;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -19,8 +17,8 @@ public class RedisLockImpl implements Lock {
     private static final String LOCK_PREFIX = "redis:lock:";
     private static final long TIME = 1;
     private static final String LOCK_MSG = "OK";
-    private static final String UNLOCK_MSG = "1";
-
+    private static final String UNLOCK_MSG = "0";
+    private static final long DEFAULT_SLEEP_TIME = 100L;
     private Jedis jedis;
 
     private LockResource lockResource;
@@ -47,7 +45,17 @@ public class RedisLockImpl implements Lock {
 
     @Override
     public void lock() {
-
+        for (; ; ) {
+            String result = this.jedis.set(LOCK_PREFIX + lockResource.getResourceName(), lockResource.getNodeInfo(), SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, 10000 * TIME);
+            if (LOCK_MSG.equals(result)) {
+                break;
+            }
+            try {
+                Thread.sleep(DEFAULT_SLEEP_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -63,16 +71,28 @@ public class RedisLockImpl implements Lock {
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        long endTime = System.currentTimeMillis() + unit.toMillis(time);
+        for (; System.currentTimeMillis() < endTime; ) {
+            String result = this.jedis.set(LOCK_PREFIX + lockResource.getResourceName(), lockResource.getNodeInfo(), SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, 10000 * TIME);
+            if (LOCK_MSG.equals(result)) {
+                return true;
+            }
+            try {
+                Thread.sleep(DEFAULT_SLEEP_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
     @Override
     public void unlock() {
         //lua script
-        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 1 end";
-        Object result ;
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result;
         result = jedis.eval(script, Collections.singletonList(LOCK_PREFIX + lockResource.getResourceName()), Collections.singletonList(lockResource.getNodeInfo()));
-        if (!UNLOCK_MSG.equals(result)){
+        if (UNLOCK_MSG.equals(result.toString())) {
             System.out.println("Unlock failure");
         }
     }
--- src/test/java/com/hza/lock/LockApplicationTests.java
+++ src/test/java/com/hza/lock/LockApplicationTests.java
@@ -15,9 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 
+import javax.swing.tree.TreeCellEditor;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
@@ -45,9 +47,9 @@ public class LockApplicationTests {
         for (int i = 0; i < 2; i++) {
             new Thread(() -> {
                 MysqlLockImpl mysqlLock = new MysqlLockImpl("a123", lockResourceService);
-                boolean ret  = false;
+                boolean ret = false;
                 ret = mysqlLock.tryLock(1L, TimeUnit.SECONDS);
-                if(ret){
+                if (ret) {
                     c += 3;
                     System.out.println(Thread.currentThread().getId() + "-aaaaa-" + c);
                     c -= 3;
@@ -85,7 +87,7 @@ public class LockApplicationTests {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if(ret){
+                if (ret) {
                     c += 3;
                     System.out.println(lock.getHoldCount() + "-aaaaa-" + c);
                     c -= 3;
@@ -132,14 +134,88 @@ public class LockApplicationTests {
 
     @Test
     public void redisLockTest() {
-        Jedis jedis = new Jedis("127.0.0.1",6379);
-        RedisLockImpl redisLock = new RedisLockImpl("a123",jedis);
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        RedisLockImpl redisLock = new RedisLockImpl("a123", jedis);
         boolean b = redisLock.tryLock();
         redisLock.unlock();
         boolean c = redisLock.tryLock();
         //redisLock.unlock();
         boolean d = redisLock.tryLock();
-        System.out.println(b+":"+c+":"+d);
+        System.out.println(b + ":" + c + ":" + d);
     }
 
+    @Test
+    public void redisLockTest2() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                RedisLockImpl redisLock = new RedisLockImpl("a123", jedis);
+                redisLock.lock();
+                redisLock.unlock();
+                redisLock.lock();
+                //redisLock.unlock();
+                boolean b = redisLock.tryLock();
+                c += 3;
+                System.out.println(Thread.currentThread().getId() + "-aaaaa-" + c);
+                c -= 3;
+                System.out.println(b);
+                redisLock.unlock();
+            }).start();
+            try {
+                Thread.sleep(200L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void redisLockTest3() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                RedisLockImpl redisLock = new RedisLockImpl("a123", jedis);
+                boolean b = false;
+                b = redisLock.tryLock();
+                if(b){
+                    c += 3;
+                    try {
+                        Thread.sleep(100L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getId() + "-aaaaa-" + c);
+                    c -= 3;
+                    //System.out.println(b);
+                    redisLock.unlock();
+                }
+            }).start();
+            try {
+                Thread.sleep(200L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void redisUnlockTest() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        RedisLockImpl redisLock = new RedisLockImpl("a123", jedis);
+        /*String script = "return redis.call('del', KEYS[1])";
+        Object result;
+        result = jedis.eval(script, Collections.singletonList("a123"), Collections.singletonList(""));*/
+        redisLock.lock();
+        redisLock.unlock();
+    }
 }
